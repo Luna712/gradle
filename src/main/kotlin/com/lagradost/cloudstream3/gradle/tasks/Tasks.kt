380c3653a52c8297592794837d30ee14c1f69f59
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-01-04 15:17:24 -0700

    Try fix

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
@@ -38,22 +38,21 @@ fun registerTasks(project: Project) {
 
     val compileDex = project.tasks.register("compileDex", CompileDexTask::class.java) {
         it.group = TASK_GROUP
-
         it.pluginClassFile.set(pluginClassFile)
 
         val kotlinTask = project.tasks.findByName("compileDebugKotlin") as KotlinCompile?
-        if (kotlinTask != null) {
-            it.dependsOn(kotlinTask)
-            it.input.from(kotlinTask.destinationDirectory)
+        kotlinTask?.let {
+            it.dependsOn(compileDex)
+            it.input.from(it.destinationDirectory)
         }
 
         val javacTask = project.tasks.findByName("compileDebugJavaWithJavac") as AbstractCompile?
-        if (javacTask != null) {
-            it.dependsOn(javacTask)
-            it.input.from(javacTask.destinationDirectory)
+        javacTask?.let {
+            it.dependsOn(compileDex)
+            it.input.from(it.destinationDirectory)
         }
 
-        it.outputFile.set(intermediates.resolve("classes.dex"))
+        it.outputFile.set(project.layout.buildDirectory.dir("intermediates/classes.dex"))
     }
 
     val compileResources = project.tasks.register("compileResources", CompileResourcesTask::class.java) {
