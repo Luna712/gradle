fcd6fa7bb95d345f489ec6254c73236c4bf7f9ec
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-13 12:20:53 -0700

    Add support for configuration cache in MakePluginsJsonTask

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
@@ -1,30 +1,27 @@
 package com.lagradost.cloudstream3.gradle.tasks
 
-import com.lagradost.cloudstream3.gradle.findCloudstream
 import com.lagradost.cloudstream3.gradle.makePluginEntry
 import com.lagradost.cloudstream3.gradle.entities.PluginEntry
 import groovy.json.JsonBuilder
 import groovy.json.JsonGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.ListProperty
 import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 import java.util.LinkedList
-import java.lang.Thread
 
 abstract class MakePluginsJsonTask : DefaultTask() {
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
+    @get:Input
+    abstract val pluginEntries: ListProperty<PluginEntry>
+
     @TaskAction
     fun makePluginsJson() {
-        val lst = LinkedList<PluginEntry>()
-
-        for (subproject in project.allprojects) {
-            subproject.extensions.findCloudstream() ?: continue
-
-            lst.add(subproject.makePluginEntry())
-        }
+        val lst = LinkedList(pluginEntries.get())
 
         outputFile.asFile.get().writeText(
             JsonBuilder(
