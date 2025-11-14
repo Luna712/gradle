3d04ffa16042729c959f5d63a7e07f5620d773ce
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-14 15:45:21 -0700

    Update

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/Tasks.kt
@@ -105,7 +105,12 @@ fun registerTasks(project: Project) {
         task.hasCrossPlatformSupport.set(extension.isCrossPlatform)
         task.pluginClassFile.set(pluginClassFile)
         task.pluginClassName.set(extension.pluginClassName)
-        task.jarInputFile.set(jarTask.flatMap { it.archiveFile })
+        task.jarInputFile.set(
+            jarTask.flatMap {
+                val single = it.outputs.files.singleFile
+                project.layout.file(single)
+            }
+        )
         task.targetJarFile.set(project.layout.buildDirectory.file("${project.name}.jar"))
         task.jarFileSize.set(extension.jarFileSize)
         
