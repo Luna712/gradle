fd33f31997ce41ebdb1b67f6563bf16452230241
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-24 15:26:02 -0700

    Add mainResSrcDir also

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
@@ -4,17 +4,18 @@ import com.android.build.gradle.BaseExtension
 import com.android.build.api.dsl.LibraryExtension
 import com.android.build.api.variant.LibraryAndroidComponentsExtension
 import org.gradle.api.Project
+import java.io.File
 
 /**
  * Compatibility layer for AGP 9, maintaining backward compatibility with AGP 8
- * for Android library modules. Provides access to minSdk and bootClasspath
- * in a way that works across both versions.
+ * for Android library modules. Provides access to minSdk, bootClasspath,
+ * and the main res source directory in a way that works across both versions.
  *
  * This class can be removed once support for AGP 8 is no longer required.
  */
 internal class LibraryExtensionCompat(private val project: Project) {
 
-    val android: Any
+    private val android: Any
         get() = project.extensions.findByName("android")
             ?: error("Android plugin not found")
 
@@ -33,6 +34,13 @@ internal class LibraryExtensionCompat(private val project: Project) {
                 ?.sdkComponents
                 ?.bootClasspath
                 ?: error("LibraryAndroidComponentsExtension not found")
+            else -> error("Unknown Android extension type")
+        }
+
+    val mainResSrcDir: File
+        get() = when (android) {
+            is BaseExtension -> android.sourceSets.getByName("main").res.srcDirs.single()
+            is LibraryExtension -> android.sourceSets.getByName("main").res.srcDirs.single()
             else -> error("Unknown Android extension type")
         }
 }
