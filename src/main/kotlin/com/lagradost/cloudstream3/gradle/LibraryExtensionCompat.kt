832ef8f54a4f4ad0df659aaa52dafdb0a58ea7df
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-24 16:55:50 -0700

    Fix?

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
@@ -46,12 +46,9 @@ internal class LibraryExtensionCompat(private val project: Project) {
 
                 var dir: File? = null
                 libraryComponents.onVariants { variant ->
-                    val resSources = variant.sources.res
-                    dir = resSources
-                    /*if (resSources != null) {
-                        val files = resSources.asFileTree.files
-                        if (files.isNotEmpty()) dir = files.first()
-                    }*/
+                    dir = variant.sources.res?.static?.map { res ->
+                        res.flatten().map { it.asFile }
+                    }
                 }
 
                 dir ?: error("Failed to resolve main resource directory for LibraryExtension")
