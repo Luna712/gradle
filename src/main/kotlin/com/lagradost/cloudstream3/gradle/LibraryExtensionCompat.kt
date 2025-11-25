5cf66aee69d62440c155a513e7ae63a166627a83
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-24 18:00:06 -0700

    Fix

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
@@ -39,7 +39,7 @@ internal class LibraryExtensionCompat(private val project: Project) {
     val mainResSrcDir: File
         get() = when (android) {
             is BaseExtension -> android.sourceSets.getByName("main").res.srcDirs.single()
-            is LibraryExtension -> File(project.layout.projectDirectory.dir("src/main/res"))/*{
+            is LibraryExtension -> project.layout.projectDirectory.dir("src/main/res")/*{
                 val libraryComponents = project.extensions
                     .findByType(LibraryAndroidComponentsExtension::class.java)
                 ?: error("LibraryAndroidComponentsExtension not found")
