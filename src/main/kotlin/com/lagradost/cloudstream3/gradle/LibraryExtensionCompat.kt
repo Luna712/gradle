535d04451e3c6396392383f68891fc3085739f48
Luna712 <142361265+Luna712@users.noreply.github.com>
2025-11-24 17:57:40 -0700

    Try fix

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/LibraryExtensionCompat.kt
@@ -39,7 +39,7 @@ internal class LibraryExtensionCompat(private val project: Project) {
     val mainResSrcDir: File
         get() = when (android) {
             is BaseExtension -> android.sourceSets.getByName("main").res.srcDirs.single()
-            is LibraryExtension -> {
+            is LibraryExtension -> File(project.layout.projectDirectory.dir("src/main/res"))/*{
                 val libraryComponents = project.extensions
                     .findByType(LibraryAndroidComponentsExtension::class.java)
                 ?: error("LibraryAndroidComponentsExtension not found")
@@ -54,7 +54,7 @@ internal class LibraryExtensionCompat(private val project: Project) {
                 }
 
                 dir ?: error("Failed to resolve main resource directory for LibraryExtension")
-            }
+            }*/
             else -> error("Unknown Android extension type")
         }
 }
