ebfca184af745a782510d3f9a0254d5331da11dc
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-11 16:07:56 -0600

    Try

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
@@ -1,31 +1,42 @@
 package com.lagradost.cloudstream3.gradle.tasks
 
+import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+import groovy.json.JsonGenerator
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 abstract class MakePluginsJsonTask : DefaultTask() {
 
-    @get:InputFiles
-    abstract val cs3Files: ConfigurableFileCollection
+	@get:InputFiles
+	abstract val entries: ConfigurableFileCollection
 
-    @get:InputFiles
-    abstract val jarFiles: ConfigurableFileCollection
+	@get:OutputFile
+	abstract val outputFile: RegularFileProperty
 
-    @get:Input
-    abstract val entriesJson: Property<String>
+	@TaskAction
+	fun run() {
+		val parser = JsonSlurper()
 
-    @get:OutputFile
-    abstract val outputFile: RegularFileProperty
+		val list = entries.files
+			.filter { it.exists() }
+			.map { file ->
+				parser.parse(file) as Any
+			}
 
-    @TaskAction
-    fun run() {
-        outputFile.get().asFile.writeText(entriesJson.get())
-        logger.lifecycle("Created ${outputFile.get().asFile}")
-    }
+		val json = JsonBuilder(
+			list,
+			JsonGenerator.Options()
+				.excludeNulls()
+				.build()
+		).toPrettyString()
+
+		outputFile.get().asFile.writeText(json)
+
+		logger.lifecycle("Created ${outputFile.get().asFile}")
+	}
 }
