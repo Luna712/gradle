27e8606f229bea94bccd2baa85e978ef01586ef0
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-11 15:18:17 -0600

    Try

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/MakePluginsJsonTask.kt
@@ -1,25 +1,36 @@
 package com.lagradost.cloudstream3.gradle.tasks
 
+import com.lagradost.cloudstream3.gradle.entities.PluginEntry
+import com.lagradost.cloudstream3.gradle.makePluginEntry
+import groovy.json.JsonBuilder
+import groovy.json.JsonGenerator
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import java.io.File
 
 abstract class MakePluginsJsonTask : DefaultTask() {
+
+    @get:InputFiles
+    abstract val cs3Files: ConfigurableFileCollection
+
+    @get:InputFiles
+    abstract val jarFiles: ConfigurableFileCollection
+
+    @get:Input
+    abstract val entriesJson: Property<String>
+
     @get:OutputFile
     abstract val outputFile: RegularFileProperty
 
-    @get:Input
-    abstract val pluginEntriesJson: Property<String>
-
     @TaskAction
-    fun makePluginsJson() {
-        outputFile.asFile.get().writeText(
-            pluginEntriesJson.get()
-        )
-
-        logger.lifecycle("Created ${outputFile.asFile.get()}")
+    fun run() {
+        outputFile.get().asFile.writeText(entriesJson.get())
+        logger.lifecycle("Created ${outputFile.get().asFile}")
     }
 }
