059658cd2ba8b6b7dc319adc99932b434fdd342a
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 14:54:35 -0600

    -

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/Utils.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/Utils.kt
@@ -1,7 +1,5 @@
 package com.lagradost.cloudstream3.gradle
 
-import org.gradle.api.Project
-import com.lagradost.cloudstream3.gradle.entities.PluginEntry
 import java.io.File
 import java.security.MessageDigest
 
@@ -17,36 +15,4 @@ fun sha256(file: File): String {
         }
     }
     return "sha256-" + digest.digest().joinToString("") { "%02x".format(it) }
-}
-
-fun makePluginEntry(
-    name: String,
-    version: Int?,
-    fileSize: Long?,
-    fileHash: String?,
-    jarFileSize: Long?,
-    jarHash: String?,
-    extension: CloudstreamExtension,
-): PluginEntry {
-    val repo = extension.repository
-    return PluginEntry(
-        url = (if (repo == null) "" else repo.getRawLink("${name}.cs3", extension.buildBranch)),
-        status = extension.status,
-        version = version ?: -1,
-        name = name,
-        internalName = name,
-        authors = extension.authors,
-        description = extension.description,
-        repositoryUrl = repo?.url,
-        language = extension.language,
-        iconUrl = extension.iconUrl,
-        apiVersion = extension.apiVersion,
-        tvTypes = extension.tvTypes,
-        fileSize = fileSize,
-        jarFileSize = jarFileSize,
-        jarUrl = (if (repo == null || jarFileSize == null) null
-                  else repo.getRawLink("${name}.jar", extension.buildBranch)),
-        jarHash = jarHash,
-        fileHash = fileHash
-    )
 }
