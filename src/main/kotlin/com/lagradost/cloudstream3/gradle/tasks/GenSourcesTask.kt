d3c12004bea537c5d8b8eb5574f9e5e4f707eb42
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 10:57:59 -0600

    Update

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/GenSourcesTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/GenSourcesTask.kt
@@ -1,22 +1,29 @@
 package com.lagradost.cloudstream3.gradle.tasks
 
-import com.lagradost.cloudstream3.gradle.getCloudstream
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
-import java.util.function.Function
 import java.net.URI
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
 import com.lagradost.cloudstream3.gradle.download
 import com.lagradost.cloudstream3.gradle.createProgressLogger
 
 abstract class GenSourcesTask : DefaultTask() {
-    @TaskAction
-    fun genSources() {
-        val extension = project.extensions.getCloudstream()
-        val apkinfo = extension.apkinfo!!
 
-        val sourcesJarFile = apkinfo.cache.resolve("cloudstream-sources.jar")
-        val url = URI("${apkinfo.urlPrefix}/app-sources.jar").toURL()
+	@get:Input
+	abstract val urlPrefix: Property<String>
 
-        url.download(sourcesJarFile, createProgressLogger(project, "Download sources"))
-    }
+	@get:OutputFile
+	abstract val sourcesJarFile: RegularFileProperty
+
+	@TaskAction
+	fun genSources() {
+		val url = URI("${urlPrefix.get()}/app-sources.jar").toURL()
+		url.download(
+			sourcesJarFile.get().asFile,
+			createProgressLogger(project, "Download sources")
+		)
+	}
 }
