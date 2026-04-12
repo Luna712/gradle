eafd6b49482e561bb3c2b00d35831e313e5cbe81
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 16:35:03 -0600

    Update

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/WriteCacheEntryTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/WriteCacheEntryTask.kt
@@ -8,7 +8,11 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
-import org.gradle.api.tasks.*
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
 
 abstract class WriteCacheEntryTask : DefaultTask() {
 
