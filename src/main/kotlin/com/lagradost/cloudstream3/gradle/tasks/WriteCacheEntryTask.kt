477645697c746b0f813a31faf6357fe317246e51
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 14:52:39 -0600

    Fix

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/WriteCacheEntryTask.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/tasks/WriteCacheEntryTask.kt
@@ -2,6 +2,7 @@ package com.lagradost.cloudstream3.gradle.tasks
 
 import com.lagradost.cloudstream3.gradle.entities.PluginEntry
 import com.lagradost.cloudstream3.gradle.makePluginEntry
+import com.lagradost.cloudstream3.gradle.sha256
 import groovy.json.JsonBuilder
 import groovy.json.JsonGenerator
 import org.gradle.api.DefaultTask
