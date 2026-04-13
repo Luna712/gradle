8e74e12184f5b85d77d233ced6bfcfac067f8776
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-13 10:26:47 -0600

    Try

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/ProjectExtensions.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/ProjectExtensions.kt
@@ -4,9 +4,10 @@ import com.android.build.api.dsl.LibraryExtension
 import com.android.build.gradle.BaseExtension
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+    extensions.getCloudstream().configuration()
 
 fun Project.android(configuration: LibraryExtension.() -> Unit) {
     extensions.getByName<LibraryExtension>("android").apply {
