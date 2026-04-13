2b6a38c288abccff80fe9b8ae1019fedf3b80c90
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 23:41:18 -0600

    -

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/CloudstreamPlugin.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/CloudstreamPlugin.kt
@@ -4,18 +4,13 @@ import com.lagradost.cloudstream3.gradle.configuration.registerConfigurations
 import com.lagradost.cloudstream3.gradle.tasks.registerTasks
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.internal.logging.progress.ProgressLoggerFactory
-import javax.inject.Inject
 
 abstract class CloudstreamPlugin : Plugin<Project> {
-
-    @get:Inject
-    abstract val progressLoggerFactory: ProgressLoggerFactory
 
     override fun apply(project: Project) {
         project.extensions.create("cloudstream", CloudstreamExtension::class.java, project)
 
         registerTasks(project)
-        registerConfigurations(project, progressLoggerFactory)
+        registerConfigurations(project)
     }
 }
