e4177c88ba5a105e55e0a208fc1a42848dce390e
Luna712 <142361265+Luna712@users.noreply.github.com>
2026-04-12 23:43:53 -0600

    Update

--- src/main/kotlin/com/lagradost/cloudstream3/gradle/configuration/CloudstreamConfigurationProvider.kt
+++ src/main/kotlin/com/lagradost/cloudstream3/gradle/configuration/CloudstreamConfigurationProvider.kt
@@ -1,35 +1,43 @@
 package com.lagradost.cloudstream3.gradle.configuration
 
 import com.lagradost.cloudstream3.gradle.ApkInfo
-import com.lagradost.cloudstream3.gradle.createProgressLogger
 import com.lagradost.cloudstream3.gradle.download
 import com.lagradost.cloudstream3.gradle.getCloudstream
-import groovy.json.JsonSlurper
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
-import java.lang.Integer.parseInt
+import org.gradle.internal.logging.progress.ProgressLoggerFactory
 import java.net.URI
-import java.nio.file.Files
+import javax.inject.Inject
 
-class CloudstreamConfigurationProvider : IConfigurationProvider {
+abstract class CloudstreamConfigurationProvider : IConfigurationProvider {
 
     override val name: String
         get() = "cloudstream"
+
+    @get:Inject
+    abstract val progressLoggerFactory: ProgressLoggerFactory
 
     override fun provide(project: Project, dependency: Dependency) {
         val extension = project.extensions.getCloudstream()
         if (extension.apkinfo == null) {
             extension.apkinfo = ApkInfo(extension, dependency.version ?: "pre-release")
         }
+
         val apkinfo = extension.apkinfo!!
-
         apkinfo.cache.mkdirs()
-
         if (!apkinfo.jarFile.exists()) {
-            project.logger.lifecycle("Fetching JAR")
-
+            project.logger.lifecycle("Fetching JAR: ${apkinfo.jarFile.name}")
             val url = URI("${apkinfo.urlPrefix}/classes.jar").toURL()
-            url.download(apkinfo.jarFile, createProgressLogger(project, "Download JAR"))
+            val progressLogger = progressLoggerFactory.newOperation("Download JAR").apply {
+                description = "Download JAR"
+            }
+
+            try {
+                progressLogger.started()
+                url.download(apkinfo.jarFile, progressLogger)
+            } finally {
+                progressLogger.completed()
+            }
         }
 
         project.dependencies.add("compileOnly", project.files(apkinfo.jarFile))
